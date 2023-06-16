c69cd7c19c5c06cacad5e8da0e42b59e38cb075f
invisloa <91217120+invisloa@users.noreply.github.com>
2023-06-16 21:31:46 +0200

    revert to json

--- profZawadzkiLesson1/Form1.cs
+++ profZawadzkiLesson1/Form1.cs
@@ -1,86 +1,110 @@
-﻿using System.Windows.Forms;
+using System.Windows.Forms;
 using System.Text.RegularExpressions;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
 using System;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using System.Xml.Linq;
+using System.Text.Json;
 
 namespace profZawadzkiLesson1
 {
 	public partial class Form1 : Form
 	{
-		string name;
-		string lastName;
-		int employeeId;
-		string department;
-		public List<Employee> EmployeesList = new List<Employee>();
-		public List<Employee> SearchList = new List<Employee>();
+
+		StudentsListGenerator listGenerator = new StudentsListGenerator();
+		public List<Student> StudentsList = new List<Student>();
+		public List<Student> SearchList = new List<Student>();
 		public Form1()
 		{
 			InitializeComponent();
-			dgvStudentsList.ColumnCount = 5;
+			dgvStudentsList.ColumnCount = 4;
 			dgvStudentsList.RowCount = 1;
 			dgvStudentsList.Columns[0].HeaderText = "lp.";
 			dgvStudentsList.Columns[0].Width = 50;
-			dgvStudentsList.Columns[1].HeaderText = "Imie";
-			dgvStudentsList.Columns[2].HeaderText = "Nazwisko";
-			dgvStudentsList.Columns[3].HeaderText = "Id";
-			dgvStudentsList.Columns[4].HeaderText = "Deparetament";
+			dgvStudentsList.Columns[1].HeaderText = "Name";
+			dgvStudentsList.Columns[2].HeaderText = "Last Name";
+			dgvStudentsList.Columns[3].HeaderText = "Students Id";
+
+			StudentsList = listGenerator.MakeStudentsList();
 		}
+
 		private void btnSumbit_Click(object sender, EventArgs e)
 		{
-			if (CanSaveCurrentInputData())
+			string name = txtBoxName.Text;
+			string lastName = txtBoxLastName.Text;
+			int studenstId;
+			if (string.IsNullOrEmpty(name))
 			{
-				if ((EmployeesList.FirstOrDefault(x => x.employeeID == employeeId)) == null)
-				{
-					EmployeesList.Add(new Employee(name, lastName, employeeId, department));
-					ClearTxtControls();
-					ShowEmployeesFromList(EmployeesList);
-					MessageBox.Show($"Dodano pracownika: {employeeId}");
-				}
-				else
-				{
-					MessageBox.Show($"Pracownik o nr ID: {employeeId} już istnieje");
-				}
+				MessageBox.Show("Name is required.");
+				return;
 			}
+
+			if (string.IsNullOrEmpty(lastName))
+			{
+				MessageBox.Show("Last name is required.");
+				return;
+			}
+			if (!int.TryParse(txtBoxStudentsId.Text, out studenstId))
+			{
+				MessageBox.Show("ID is required.");
+				return;
+			}
+
+
+			if (SearchBox.Text == "")
+			{
+				StudentsList.Add(new Student(name, lastName, studenstId));
+			}
+			else if (SearchBox.Text.Length > 0 && dgvStudentsList.Rows.Count == 1)
+			{
+				int searcherdID = Int32.Parse((dgvStudentsList.Rows[0].Cells[3].Value).ToString());
+				var studentToChange = StudentsList.FirstOrDefault(x => x.StudentId == searcherdID);
+				studentToChange.StudentId = studenstId;
+				studentToChange.Name = name;
+				studentToChange.LastName = lastName;
+			}
+
+			ClearTxtControls();
+		}
+		private void btnShowStudents_Click(object sender, EventArgs e)
+		{
+			ShowStudentsFromList(StudentsList);
 		}
 		void ClearTxtControls()
 		{
 			txtBoxName.Text = string.Empty;
 			txtBoxLastName.Text = string.Empty;
 			txtBoxStudentsId.Text = string.Empty;
-			DepertmentTextBox.Text = string.Empty;
-			SearchBox.Text = string.Empty;
 		}
+
 		private void SearchBox_TextChanged(object sender, EventArgs e)
 		{
 			SearchList.Clear();
-			SearchList = EmployeesList.Where(x => x.employeeID.ToString().Contains(SearchBox.Text.ToLower())).ToList();
-			ShowEmployeesFromList(SearchList);
+			SearchList = StudentsList.Where(x => x.Name.ToLower().Contains(SearchBox.Text.ToLower())
+											|| x.LastName.ToLower().Contains(SearchBox.Text.ToLower())
+											|| x.StudentId.ToString().Contains(SearchBox.Text.ToLower())).ToList();
+			ShowStudentsFromList(SearchList);
 		}
-		private void SearchBox_KeyPress(object sender, KeyPressEventArgs e)
+		private void SearchBox_LostFocus(object sender, EventArgs e)
 		{
-			if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-			{
-				e.Handled = true;
-			}
+			SearchBox.Clear();
+
 		}
+
+
 		private void btnClearStudentsList_Click(object sender, EventArgs e)
 		{
-			EmployeesList.Clear();
+			StudentsList.Clear();
 			dgvStudentsList.Rows.Clear();
-			ClearTxtControls();
 		}
-		void ShowEmployeesFromList(List<Employee> employeesList)
+		void ShowStudentsFromList(List<Student> studentsList)
 		{
 			dgvStudentsList.Rows.Clear();
 			int currentLpNumber = 1;
-			if (employeesList != null)
+			if (studentsList != null)
 			{
-				foreach (Employee student in employeesList)
+				foreach (Student student in studentsList)
 				{
 					string[] tab = student.ToString().Split(' ');
 					string[] rowItems = new string[] { currentLpNumber.ToString() }.Concat(tab).ToArray();
@@ -89,73 +113,38 @@ namespace profZawadzkiLesson1
 				}
 			}
 		}
-		void UpdateEmployeeData()
-		{
-			if (SearchList.Count == 1 && SearchBox.Text != "")
-			{
-				if (CanSaveCurrentInputData())
-				{
-					int searchedEmployeeID = Int32.Parse((dgvStudentsList.Rows[0].Cells[3].Value).ToString());
-					var employeeToChange = EmployeesList.FirstOrDefault(x => x.employeeID == searchedEmployeeID);
-					employeeToChange.employeeID = employeeId;
-					employeeToChange.Name = name;
-					employeeToChange.LastName = lastName;
-					employeeToChange.Department = department;
-					MessageBox.Show($"Aktualizacja studenta o nr ID: {employeeId}");
-				}
-			}
-			else
-			{
-				MessageBox.Show("Niepoprawne dane do aktualizacji");
-				ClearTxtControls();
-			}
-		}
-		bool CanSaveCurrentInputData()
-		{
-			name = txtBoxName.Text;
-			lastName = txtBoxLastName.Text;
-			department = DepertmentTextBox.Text;
-			if (string.IsNullOrEmpty(txtBoxName.Text))
-			{
-				MessageBox.Show("Podaj poprawne imię");
-				return false;
-			}
 
-			if (string.IsNullOrEmpty(txtBoxLastName.Text))
-			{
-				MessageBox.Show("Podaj poprawne nazwisko");
-				return false;
-			}
-			if (!int.TryParse(txtBoxStudentsId.Text, out employeeId))
-			{
-				MessageBox.Show("Podaj poprawne ID");
-				return false;
-			}
-			if (string.IsNullOrEmpty(DepertmentTextBox.Text))
-			{
-				MessageBox.Show("Podaj poprawny departament");
-				return false;
-			}
-			ClearTxtControls();
-			return true;
-		}
 		private void button2_Click(object sender, EventArgs e)
 		{
+			string jsonString = JsonSerializer.Serialize(StudentsList);
 
+			// Write the JSON string to a file
+			File.WriteAllText("listaJson.json", jsonString);
 		}
+
 		private void button3_Click(object sender, EventArgs e)
 		{
+			string jsonString = File.ReadAllText("listaJson.json");
+
+			// Convert the JSON string to a List
+			List<Student> list = JsonSerializer.Deserialize<List<Student>>(jsonString);
+
+						ShowStudentsFromList(list);
+
+
+
+
 		}
 
-		private void btnAktualizuj_Click(object sender, EventArgs e)
+		string AllStudentsString(List<Student> studentsList)
 		{
-			UpdateEmployeeData();
-			ClearTxtControls();
-		}
+			var studentsString = string.Empty;
+			foreach (var item in studentsList)
+			{
+				studentsString += item.ToString() + "\n";
 
-		private void btnShowAll_Click(object sender, EventArgs e)
-		{
-			ShowEmployeesFromList(EmployeesList);
+			}
+			return studentsString;
 		}
 	}
 }
